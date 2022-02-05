621fa8e726e634de8568210d5f11bb0e4cd8d6ca
Daniel Gonzalez <danielhumgon@gmail.com>
2022-02-04 22:46:43 -0400

    fix(menu): Restored Sweep menu item

--- src/components/sweep/sweep-scanner.js
+++ src/components/sweep/sweep-scanner.js
@@ -3,10 +3,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Content, Row, Col, Box, Button } from 'adminlte-2-react'
-import 'gatsby-theme-bch-wallet/src/components/qr-scanner/qr-scanner.css'
+import 'fullstack-gatsby-theme-bch-wallet/src/components/qr-scanner/qr-scanner.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import QrReader from 'react-qr-reader'
-// import { getWalletInfo } from 'gatsby-theme-bch-wallet/src/components/localWallet'
+// import { getWalletInfo } from 'fullstack-gatsby-theme-bch-wallet/src/components/localWallet'
 
 import './sweep.css'
 let _this
